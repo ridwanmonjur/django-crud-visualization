9e734c17d7db5cda9c7c84e4ef24db7e9612a6eb
Ridwan Monjur <mjrrdn@gmail.com>
2023-06-07 21:32:11 +0800

    finished the non-database views

--- trade/urls.py
+++ trade/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("delete/<int:index_delete>", views.delete, name="delete"),
+    path("add", views.add),
+    path("update/<int:index_update>", views.update),
+    path("do-update/<int:index_update>", views.do_update),
 ]
--- trade/views.py
+++ trade/views.py
@@ -1,11 +1,60 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import json
 
 with open('./stock_market_data.json', 'r') as file:
-    trades = json.load(file)
+    stocks = json.load(file)
+stocksPaginated = stocks[0:20]
 
 
 # Create your views here.
 def index(request):
-    return render(request, "trade/index.html", {'trades': trades})
+    print(stocksPaginated[0])
+    return render(request, "trade/index.html", {'stocks': stocksPaginated})
+
+
+def delete(request, index_delete):
+    del stocksPaginated[index_delete]
+    return redirect("/")
+
+
+def add(request):
+    if request.method == "POST":
+        date = request.POST.get("date")
+        high = request.POST.get("high")
+        low = request.POST.get("low")
+        trade_code = request.POST.get("trade_code")
+        open = request.POST.get("open")
+        close = request.POST.get("close")
+        volume = request.POST.get("volume")
+        stocksPaginated.append(
+            {'date': date, 'high': high, 'low': low, 'trade_code': trade_code, 'open': open,
+             'close': close, 'volume': volume})
+        # stocks.append({'date': date, 'high': high, 'low': low, 'trade_code': trade_code, 'open': open,
+        #                                          'close': close, 'volume': volume})
+        return redirect("/")
+    return render(request, "trade/add.html", {})
+
+
+def update(request, index_update):
+    stock = stocks[index_update]
+    return render(request, "trade/update.html", {
+        'stock': stock, 'stockIndex': index_update
+    })
+
+
+def do_update(request, index_update):
+    if request.method == "POST":
+        date = request.POST.get("date")
+        high = request.POST.get("high")
+        low = request.POST.get("low")
+        trade_code = request.POST.get("trade_code")
+        open = request.POST.get("open")
+        close = request.POST.get("close")
+        volume = request.POST.get("volume")
+        stocksPaginated[index_update] = {'date': date, 'high': high, 'low': low, 'trade_code': trade_code, 'open': open,
+                                         'close': close, 'volume': volume}
+        print(stocksPaginated[index_update])
+        stocks[index_update] = {'date': date, 'high': high, 'low': low, 'trade_code': trade_code, 'open': open,
+                                'close': close, 'volume': volume}
+    return redirect("/")
