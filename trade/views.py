588151cc8cae9e1b83d04abaa9381ef42b37f924
Ridwan Monjur <mjrrdn@gmail.com>
2023-06-07 18:39:03 +0800

    loaded data, added bootstrap and did the index view

--- trade/views.py
+++ trade/views.py
@@ -1,6 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import json
+
+with open('./stock_market_data.json', 'r') as file:
+    trades = json.load(file)
+
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the trade index.")
+    return render(request, "trade/index.html", {'trades': trades})
